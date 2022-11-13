481c698d1d68c930d171f7998b427654036005ef
Izirae <lautaro.bazzola@gmail.com>
2022-11-13 11:14:20 -0300

    first commit

---
+++ src/utils/colors.js
@@ -0,0 +1,11 @@
+export default {
+  primary: "#660708",
+  secondary: "#B1A7A6",
+  tertiary: "#A4161A",
+  background: "#161A1D",
+  text: "#F5F3F4",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#808080",
+  placeholder: "#4A4A48"
+};
